f6679a1d7133b09b1aca8a09904d2e0620bbd85b
vinidotruan <contatoruanvinicius@gmail.com>
2021-02-01 17:51:08 -0300

    - Add spotify login

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const clientID = "d0d8d623fec94fceb189a1a6bd05eff4";
+
 const livereload = require("livereload");
 const path = require('path');
 const connectLivereload = require("connect-livereload");
@@ -14,9 +16,13 @@ liveReloadServer.server.once("connection", () => {
     setTimeout(() => {
       liveReloadServer.refresh("/");
     }, 100);
-  });
+});
+// imports
+app.use('/ness', express.static(__dirname + '/node_modules/nes.css/css/'));
+app.use('/css', express.static(__dirname + '/public/css/'));
 
 
+// routes
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
@@ -24,6 +30,18 @@ app.get('/', function(req, res) {
 app.get('/home', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/home/index.html'));
 });
+
+app.get('/login', function(req, res) {
+    var scopes = 'user-read-private user-read-email';
+    var redirect_uri = 'http://localhost:3000/home';
+    console.log(res)
+    res.redirect('https://accounts.spotify.com/authorize' +
+      '?response_type=code' +
+      '&client_id=' + clientID +
+      (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+      '&redirect_uri=' + encodeURIComponent(redirect_uri));
+});
+
 
 app.listen(3000, () => {
     console.log(`Server hosting at: http://localhost:3000`)
